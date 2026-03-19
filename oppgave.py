c4b044851f99a28766c8e9f7ccaf79c1a8f42b26
Enes Berk <126110427+EnesB3rk@users.noreply.github.com>
2026-03-19 20:24:17 +0100

    Legg til funksjon og kritisk ligning for oppgave 1

--- oppgave.py
+++ oppgave.py
@@ -1,1 +1,13 @@
+import math
 
+# f(x)
+def f(x):
+    return math.exp(-x/4) * math.atan(x)
+
+# g(x) = kritisk ligning (fra analytisk del)
+def g(x):
+    return math.atan(x) - 4/(x**2 + 1)
+
+
+if __name__ == "__main__":
+    print("Funksjonene er definert. Klar for numerisk løsning.")
